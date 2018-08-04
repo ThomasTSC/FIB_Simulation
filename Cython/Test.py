257e737e3324807654363361225f18936eb61cb5
ThomasTSC <tashunchou@gmail.com>
2018-08-04 08:28:21 +0200

    Cython test finished

---
+++ Cython/Test.py
@@ -0,0 +1,9 @@
+'''
+Created on Aug 4, 2018
+
+@author: thoma
+'''
+
+import Hello
+
+
--- Cython/setup.py
+++ Cython/setup.py
@@ -3,3 +3,8 @@ Created on Aug 3, 2018
 
 @author: thoma
 '''
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(name='Hello world app',
+      ext_modules=cythonize("Hello.pyx"))
